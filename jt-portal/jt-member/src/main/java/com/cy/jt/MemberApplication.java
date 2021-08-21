1876b4e9ced43d1c71d254a54d33498e227f983b
gaoxiaoyay123 <1484461900@qq.com>
2021-08-21 14:32:25 +0800

    create jt

---
+++ jt-portal/jt-member/src/main/java/com/cy/jt/MemberApplication.java
@@ -0,0 +1,11 @@
+package com.cy.jt;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberApplication {
+    public static void main(String[] args) {
+
+
+    }
+}
